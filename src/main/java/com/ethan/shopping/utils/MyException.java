f285ef37ee5abfbeb3416fb309a7c6d503ebdebf
ethan <wangyx029@outlook.com>
2019-07-22 23:30:03 +0800

    修改异常类型

--- src/main/java/com/ethan/shopping/utils/MyException.java
+++ src/main/java/com/ethan/shopping/utils/MyException.java
@@ -1,6 +1,6 @@
 package com.ethan.shopping.utils;
 
-public class MyException extends Exception {
+public class MyException extends RuntimeException {
     public MyException(){
         super();
     }
